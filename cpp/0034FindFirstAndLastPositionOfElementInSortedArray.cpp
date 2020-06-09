77acefbc01c3d39793ec4f7887fd0fb0388a1b45
Torrey <torrey.tsui@outlook.com>
2020-06-09 12:49:25 +0800

    add No.704二分搜索  No.34排序数组中第一个和最后一个位置

---
+++ cpp/0034FindFirstAndLastPositionOfElementInSortedArray.cpp
@@ -0,0 +1,180 @@
+/*
+ * @Description: 
+ * @Author: 徐小明
+ * @Date: 2020-06-08 16:55:37
+ * @LastEditors: 徐小明
+ * @LastEditTime: 2020-06-08 21:06:40
+ * @FilePath: \cpp\0034FindFirstAndLastPositionOfElementInSortedArray.cpp
+ */
+// 34. 在排序数组中查找元素的第一个和最后一个位置
+// 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+// 你的算法时间复杂度必须是 O(log n) 级别。
+// 如果数组中不存在目标值，返回 [-1, -1]。
+// 示例 1:
+// 输入: nums = [5,7,7,8,8,10], target = 8
+// 输出: [3,4]
+
+// 示例 2:
+// 输入: nums = [5,7,7,8,8,10], target = 6
+// 输出: [-1,-1]
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
+        vector<int> res;
+        res.push_back(left_bound(nums, target));
+        res.push_back(right_bound(nums, target));
+        return res;
+    }
+
+private:
+    int left_bound(vector<int> &nums, int target)
+    {
+        int sz = nums.size();
+        if (sz == 0)
+        {
+            return -1;
+        }
+        int left = 0, right = sz - 1;
+        // 左闭右闭区间搜索，搜索情况划分为小于目标，等于目标，大于目标
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+            {
+                right = mid - 1; // 向左收缩
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+        }
+        if (left >= sz || nums[left] != target)
+        {
+            return -1;
+        }
+        return left;
+    }
+
+    int right_bound(vector<int> &nums, int target)
+    {
+        int sz = nums.size();
+        if (sz == 0)
+        {
+            return -1;
+        }
+        int left = 0, right = sz - 1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+            {
+                left = mid + 1; // 向右收缩
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+        }
+        if (right < 0 || nums[right] != target)
+        {
+            return -1;
+        }
+        return right;
+    }
+};
+
+class Solution2
+{
+public:
+    vector<int> searchRange(vector<int> &nums, int target)
+    {
+        vector<int> res;
+        res.push_back(left_bound(nums, target));
+        res.push_back(right_bound(nums, target));
+        return res;
+    }
+
+private:
+    int left_bound(vector<int> &nums, int target)
+    {
+        int sz = nums.size();
+        if (sz == 0)
+        {
+            return -1;
+        }
+        int left = 0, right = sz - 1;
+        // 左闭右开区间搜索，搜索情况划分为不满足，可能满足
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) // 不满足要求
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+        if (nums[left] != target)
+        {
+            return -1;
+        }
+
+        return left;
+    }
+
+    int right_bound(vector<int> &nums, int target)
+    {
+        int sz = nums.size();
+        if (sz == 0)
+        {
+            return -1;
+        }
+        int left = 0, right = sz - 1;
+        // 左闭右开区间搜索，搜索情况划分为不满足，可能满足
+        while (left < right)
+        {
+            int mid = left + (right - left + 1) / 2; // 向上取整，不然可能会死循环
+            if (nums[mid] > target)
+            {
+                right = mid - 1; // 不满足要求
+            }
+            else
+            {
+                left = mid;
+            }
+        }
+        if (nums[left] != target)
+        {
+            return -1;
+        }
+        return left;
+    }
+};
+
+int main()
+{
+    return 0;
+}
---
+++ cpp/0704BinarySearch.cpp
@@ -0,0 +1,95 @@
+/*
+ * @Description: 
+ * @Author: 徐小明
+ * @Date: 2020-06-06 13:24:44
+ * @LastEditors: 徐小明
+ * @LastEditTime: 2020-06-06 13:34:19
+ * @FilePath: \cpp\0704BinarySearch.cpp
+ */
+
+// 704. 二分查找
+// 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，
+// 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 - 1。
+// 示例 1:
+// 输入: nums = [-1,0,3,5,9,12], target = 9
+// 输出: 4
+// 解释: 9 出现在 nums 中并且下标为 4
+// 示例 2:
+// 输入: nums = [-1,0,3,5,9,12], target = 2
+// 输出: -1
+// 解释: 2 不存在 nums 中因此返回 -1
+
+// 提示：
+//     你可以假设 nums 中的所有元素是不重复的。
+//     n 将在 [1, 10000]之间。
+//     nums 的每个元素都将在 [-9999, 9999]之间。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/binary-search
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+#include <vector>
+#include <iostream>
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int sz = nums.size();
+        if (sz == 0)
+        {
+            return -1;
+        }
+
+        int left = 0, right = sz - 1;
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
+
+class Solution2
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int sz = nums.size();
+        if (sz == 0)
+        {
+            return -1;
+        }
+
+        int left = 0, right = sz - 1;
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+        return nums[left] == target ? left : -1;
+    }
+};
+
+int main()
+{
+    return 0;
+}
